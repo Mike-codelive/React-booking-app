3246dd526f143af9d204db9be018c492dd439220
Mikeresolve <txrz56ad@live.com.mx>
2021-04-16 21:06:22 -0500

    styling productimages component

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,41 +15,61 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
+  const { id } = useParams()
+  const history = useHistory()
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext()
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`)
+    // eslint-disable-next-line
+  }, [id])
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/')
+      }, 3000)
+    }
+    // eslint-disable-next-line
+  }, [error])
+
+  if (loading) {
+    return <Loading />
+  }
+  if (error) {
+    return <Error />
+  }
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product
+
+  return (
+    <Wrapper className='theme-background'>
+      <div className='max-w-center'>
+        <div className='product-show'>
+          <ProductImages images={images} />
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
-const Wrapper = styled.main`
-  .product-center {
-    display: grid;
-    gap: 4rem;
-    margin-top: 2rem;
-  }
-  .price {
-    color: var(--clr-primary-5);
-  }
-  .desc {
-    line-height: 2;
-    max-width: 45em;
-  }
-  .info {
-    text-transform: capitalize;
-    width: 300px;
-    display: grid;
-    grid-template-columns: 125px 1fr;
-    span {
-      font-weight: 700;
-    }
-  }
+const Wrapper = styled.section`
 
-  @media (min-width: 992px) {
-    .product-center {
-      grid-template-columns: 1fr 1fr;
-      align-items: center;
-    }
-    .price {
-      font-size: 1.25rem;
-    }
-  }
+
 `
 
 export default SingleProductPage
